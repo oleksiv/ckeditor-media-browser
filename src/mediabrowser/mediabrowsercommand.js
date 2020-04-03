73fcd3a09ea39985374ffa87516f3f1028565c42
Sviatoslav Oleksiv <hi.oleksiv@gmail.com>
2020-04-03 10:57:15 +0300

    prod build

--- src/mediabrowser/mediabrowsercommand.js
+++ src/mediabrowser/mediabrowsercommand.js
@@ -2,7 +2,6 @@
  * @module media/mediabrowser/mediabrowsercommand
  */
 import Command from '@ckeditor/ckeditor5-core/src/command';
-import * as filestack from "filestack-js";
 import {getTypeFromUrl} from "../media/utils";
 
 /**
@@ -16,54 +15,19 @@ export default class MediaBrowserCommand extends Command {
      */
     execute() {
         const editor = this.editor;
-        const editorDoc = editor.sourceElement.ownerDocument;
-        const apiKey = editor.config.get('fileStackApiKey');
-        if (!apiKey) {
-            console.error('fileStackApiKey is not defined in editor config');
-        }
-
-        const client = filestack.init(apiKey);
-
         editor.model.change(writer => {
-            // const browser = editor.config.get('media.browser');
-            //
-            // if (!browser || !browser.length) {
-            //     return;
-            // }
-
-            const options = {
-                onFileUploadFinished: file => {
+            if (typeof editor.config._config.openUploader === 'function') {
+                editor.config._config.openUploader((file) => {
                     const type = getTypeFromUrl(file.url);
                     editor.model.insertContent(writer.createElement('media', {
                         alt: '',
                         src: file.url,
                         type: type.id,
                     }), editor.model.document.selection);
-                },
-                fromSources: ['local_file_system'],
-                storeTo: {
-                    location: 's3',
-                    path: editor.config._config.getPath(this),
-                }
-                // accept: ['video/*'],
-            };
-
-            client.picker(options).open().then(() => {
-                // debugger;
-            });
-
-            // const src = 'https://via.placeholder.com/350x150';
-            // let type = null;
-            //
-            // if (!type && (type = getTypeFromUrl(src))) {
-            //     type = type.id;
-            // }
-            //
-            // editor.model.insertContent(writer.createElement('media', {
-            //     alt: 'test',
-            //     src: 'https://via.placeholder.com/350x150',
-            //     type: type
-            // }), editor.model.document.selection);
+                });
+            } else {
+                console.error('Please specify openUploader callback in editor config');
+            }
         });
     }
 }
